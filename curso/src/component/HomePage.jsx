0b38d00018bbf52e5185fbef665efad5a44c3078
Ramiro Gordon <ramiro.gordon@globant.com>
2019-05-14 17:15:04 -0300

    router setup

---
+++ curso/src/component/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './NavBar';
+import '../Styles/HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <NavBar />    
+      </div>
+      
+      <h1>Hi!!!</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ curso/src/component/NavBar.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import '../Styles/NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+        <ul>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+            <li>
+                <Link to="/javascript-drum-kit">JavaScript Drum Kit</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar

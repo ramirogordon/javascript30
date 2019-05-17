9bb7b829df5f8bea4392c3acafb05b2a8e1c189b
Ramiro Gordon <ramiro.gordon@globant.com>
2019-05-17 15:34:21 -0300

    poansd

--- curso/src/component/TypeAhead/TypeAhead.jsx
+++ curso/src/component/TypeAhead/TypeAhead.jsx
@@ -1,32 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TypeAhead.scss';
-import { ENODATA } from 'constants';
 
 const TypeAhead = () => {
-    // const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-    // const cities = [];
-    // fetch(endpoint)
-    //     .then(response => response.json())
-    //     .then(data => cities.push(...data));
-    // console.log(cities);
+    const endpoint     = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+    const cities       = [];
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data     => cities.push(...data));
 
-    // const findMatches = (wordToMatch, cities) => {
-    //     return cities.filter(place => {
-    //         return place.city.match
-    //     })
-    // }
+    const matches      = useDataInput(cities);
+    const suggestions  = matches.data.map((place, i) => {
+        return( 
+            <li key={i}>
+                <span className="name">{place.city}, {place.state}</span>
+                <span className="population">{place.population}</span>
+            </li>
+        );
+    });
+    
 
     return (
         <div className="type-ahead-screen">
             <form className="search-form">
-                <input type="text" className="search" placeholder="City or State of EEUU" />
+                <input type="text" className="search" defaultValue="santa" onChange={matches.displayMatches} placeholder="City or State of EEUU" />
                 <ul className="suggestions">
                     <li>Filter for a city</li>
                     <li>or a state</li>
+                    {suggestions}
                 </ul>
             </form>
         </div>
     )
+}
+
+const useDataInput = (initialState) => {
+    const [data, setData]   = useState(initialState);
+    const [input, setInput] = useState('');
+
+    const findMatches = (wordToMatch, cities) => {
+        setData(() => {
+            return cities.filter(place => {
+                const regex = new RegExp(wordToMatch, 'gi');
+                return place.city.match(regex) || place.state.match(regex);
+            })
+        });
+    }
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+        findMatches(e.target.value, initialState);
+    }
+    return {
+        data,
+        input,
+        displayMatches: handleChange
+    }
 }
 
 export default TypeAhead
